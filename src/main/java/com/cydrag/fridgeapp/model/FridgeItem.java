751d523054e11f292bcd3328282f90c8066fb180
Predrag Stefanovic <predrag.stefanovic98@gmail.com>
2025-12-06 16:10:18 +0100

    Added @Version annotation for optimistic locking

--- src/main/java/com/cydrag/fridgeapp/model/FridgeItem.java
+++ src/main/java/com/cydrag/fridgeapp/model/FridgeItem.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Version;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -44,6 +45,10 @@ public class FridgeItem {
 
     @Column(name = "best_before", nullable = false)
     private LocalDate bestBefore;
+
+    @Version
+    @Column(nullable = false)
+    private Long version;
 
     public FridgeItem(String productName, LocalDate bestBefore, Fridge fridge) {
         this.productName = productName;
